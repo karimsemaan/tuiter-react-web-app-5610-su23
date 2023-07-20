11d3208f47a7eed896f1825068781ca638c67d58
karimsemaan <semaan.k@northeastern.edu>
2023-07-20 17:37:03 +0300

    add icons to navigation bar

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FaHome, FaHashtag, FaBell, FaEnvelope, FaBookmark, FaList, FaUser, FaEllipsisH } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 const NavigationSidebar = () => {
   const { pathname } = useLocation();
   const [, , active] = pathname.split("/");
-  const links = ["home", "explore", "notifications", "messages", "bookmarks", "lists", "profile", "more"];
+  const links = [
+    { name: '', icon: <FaTwitter /> },
+    { name: 'home', icon: <FaHome /> },
+    { name: 'explore', icon: <FaHashtag /> },
+    { name: 'notifications', icon: <FaBell /> },
+    { name: 'messages', icon: <FaEnvelope /> },
+    { name: 'bookmarks', icon: <FaBookmark /> },
+    { name: 'lists', icon: <FaList /> },
+    { name: 'profile', icon: <FaUser /> },
+    { name: 'more', icon: <FaEllipsisH /> },
+  ];  
   return (
     <div className="list-group">
-      {links.map((link) =>
-        <Link to={`/tuiter/${link}`} className={`list-group-item text-capitalize ${active === link ? "active" : ""}`}>
-          {link}
+      {links.map((link) => 
+        <Link to={`/tuiter/${link.name}`} className={`list-group-item text-capitalize ${active === link.name ? "active" : ""}`}>
+          {link.icon} {link.name}
         </Link>
       )}
     </div>
