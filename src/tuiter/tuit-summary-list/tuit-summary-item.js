1a16f3ab6e87f01797f5b18575bb6acb0554b031
karimsemaan <semaan.k@northeastern.edu>
2023-08-08 09:16:37 +0300

    explore page fix

--- src/tuiter/tuit-summary-list/tuit-summary-item.js
+++ src/tuiter/tuit-summary-list/tuit-summary-item.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { MdVerified } from "react-icons/md";
 const TuitSummaryItem = (
  {
    tuit = {
      "topic": "Space",
      "userName": "SpaceX",
      "time": "2h",
-     "title": `Tesla CyberTruck lands on Mars and
+     "tuit": `Tesla CyberTruck lands on Mars and
                picks up the Curiosity rover on its 6' bed`,
      "image": "tesla-square.jpeg"
    }
@@ -15,9 +16,9 @@ const TuitSummaryItem = (
   <li className="list-group-item">
    <div className="row">
      <div className="col-10">
-       <div>{tuit.userName} . {tuit.time}</div>
+       <div>{tuit.userName} <MdVerified style={{color: "blue"}}/> {tuit.handle} &middot; {tuit.time}</div>
        <div className="fw-bolder">{tuit.topic}</div>
-       <div>{tuit.title}</div>
+       <div>{tuit.tuit}</div>
      </div>
      <div className="col-2">
        <img width={70} className="float-end rounded-3" src={`/images/${tuit.image}`} alt={tuit.userName}/>
