dbc1b39263fe1d9925a7383adb9e9a9511dc3635
karimsemaan <semaan.k@northeastern.edu>
2023-08-08 08:59:18 +0300

    changes

--- src/tuiter/tuit-summary-list/index.js
+++ src/tuiter/tuit-summary-list/index.js
@@ -1,17 +1,23 @@
-import React from "react";
+import { React, useEffect } from "react";
 import TuitSummaryItem from "./tuit-summary-item";
-import {useSelector} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { findTuitsThunk } from "../services/tuits-thunks";
 
 const TuitSummaryList = () => {
-  const { tuits } = useSelector(state => state.tuits)
- return(
-   <ul className="list-group">
-     {
-       tuits.map(tuit =>
-         <TuitSummaryItem
-           key={tuit._id} tuit={tuit}/> )
-     }
-   </ul>
- );
+  const { tuits, loading } = useSelector(state => state.tuits);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(findTuitsThunk());
+  }, [dispatch]);
+  return (
+    <ul className="list-group">
+      {loading && <li className="list-group-item">Loading...</li>}
+      {
+        tuits.map(tuit =>
+          <TuitSummaryItem
+            key={tuit._id} tuit={tuit} />)
+      }
+    </ul>
+  );
 };
 export default TuitSummaryList;
